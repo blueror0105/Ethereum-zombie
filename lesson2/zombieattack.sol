916068f04830ef71e075fad0a122bd22ff0f9ad3
Blue <91778766+bllue0313@users.noreply.github.com>
2021-12-01 19:42:21 +0100

    Added a feature to generate random number

--- lesson2/zombieattack.sol
+++ lesson2/zombieattack.sol
@@ -2,4 +2,12 @@ pragma solidity >=0.5.0 <0.8.11;
 
 import "./zombiehelper.sol";
 
-contract ZombieAttack is ZombieHelper {}
+contract ZombieAttack is ZombieHelper {
+    uint256 randNonce = 0;
+
+    function randMod(uint256 _modulus) returns (uint256) {
+        randNonce++;
+
+        return uint256(keccak256(abi.encodePacked(now, msg.sender))) % _modulus;
+    }
+}
