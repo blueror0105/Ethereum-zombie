ab177d4a01fa6d7a878c32d24f2c6bc5f46409a6
Blue <91778766+bllue0313@users.noreply.github.com>
2021-12-02 09:44:37 +0100

    Added multiple inheritance to zombieOwnership

--- lesson2/zombieownership.sol
+++ lesson2/zombieownership.sol
@@ -1,4 +1,5 @@
 pragma solidity >=0.5.0 <0.8.11;
 import "./zombieattack.sol";
+import "./erc721.sol";
 
-contract ZombieOwnership is ZombieAttack {}
+contract ZombieOwnership is ZombieAttack, ERC721 {}
